33f6580d07ac2ff798e514b5685892733bfb689b
Yashika Garg <yashikagarg05@gmail.com>
2023-08-16 11:20:44 +0530

    first-commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MyFirstApp</Text>
+
+      <Text>Deployment of react native for android and ios</Text>
       <StatusBar style="auto" />
     </View>
   );
